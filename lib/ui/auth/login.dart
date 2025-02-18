aa99e7d88d8187860a127c3de465ce460fc2197f
Nhi <nhi19603@gmail.com>
2025-02-18 15:47:09 +0700

    Xay dung trang dang nhap, dang ky

---
+++ lib/ui/auth/login.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatelessWidget {
+  const Login({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: Opacity(
+              opacity: 0.6,
+              child: Image.asset(
+                'assets/images/bg_img.jpg',
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          SingleChildScrollView(
+            child: SafeArea(
+              child: Container(
+                height: MediaQuery.of(context).size.height,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Image.asset(
+                      'assets/images/logo.png',
+                      width: 200,
+                    ),
+                    const SizedBox(height: 10),
+                    Text(
+                      "KHÁM PHÁ THẾ GIỚI, THEO CÁCH CỦA BẠN",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontWeight: FontWeight.w200,
+                        fontSize: 16,
+                      ),
+                    ),
+                    const SizedBox(height: 70),
+                    Text(
+                      "ĐĂNG NHẬP",
+                      style: TextStyle(
+                        fontSize: 28,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    Container(
+                      margin: const EdgeInsets.symmetric(horizontal: 20),
+                      padding: const EdgeInsets.all(20),
+                      decoration: BoxDecoration(
+                        color: Colors.white.withOpacity(0.8),
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          TextField(
+                            decoration: InputDecoration(
+                                labelText: "Email",
+                                border: UnderlineInputBorder(),
+                                focusedBorder: UnderlineInputBorder(
+                                  borderSide: BorderSide(
+                                      color: Theme.of(context).primaryColor),
+                                )),
+                          ),
+                          TextField(
+                            decoration: InputDecoration(
+                              labelText: "Mật khẩu",
+                              border: UnderlineInputBorder(),
+                              focusedBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(
+                                    color: Theme.of(context).primaryColor),
+                              ),
+                            ),
+                            obscureText: true,
+                          ),
+                          const SizedBox(height: 30),
+                          ElevatedButton(
+                            onPressed: () {},
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor:
+                                  Theme.of(context).colorScheme.primary,
+                              foregroundColor:
+                                  Theme.of(context).colorScheme.onPrimary,
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 30, vertical: 12),
+                            ),
+                            child: const Text("ĐĂNG NHẬP"),
+                          ),
+                          const SizedBox(height: 40),
+                          RichText(
+                            text: TextSpan(
+                              text: "Nếu bạn chưa có tài khoản hãy ",
+                              style:
+                                  TextStyle(fontSize: 14, color: Colors.black),
+                              children: const <TextSpan>[
+                                TextSpan(
+                                  text: "Đăng ký",
+                                  style: TextStyle(
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.bold,
+                                    decoration: TextDecoration.underline,
+                                    color: Colors.black,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/ui/auth/register.dart
@@ -0,0 +1,137 @@
+import 'package:flutter/material.dart';
+
+class Register extends StatelessWidget {
+  const Register({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: Opacity(
+              opacity: 0.6,
+              child: Image.asset(
+                'assets/images/bg_img.jpg',
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          SingleChildScrollView(
+            child: SafeArea(
+              child: Container(
+                height: MediaQuery.of(context).size.height,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Image.asset(
+                      'assets/images/logo.png',
+                      width: 200,
+                    ),
+                    const SizedBox(height: 10),
+                    Text(
+                      "KHÁM PHÁ THẾ GIỚI, THEO CÁCH CỦA BẠN",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontWeight: FontWeight.w200,
+                        fontSize: 16,
+                      ),
+                    ),
+                    const SizedBox(height: 70),
+                    Text(
+                      "ĐĂNG KÝ",
+                      style: TextStyle(
+                        fontSize: 28,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    Container(
+                      margin: const EdgeInsets.symmetric(horizontal: 20),
+                      padding: const EdgeInsets.all(20),
+                      decoration: BoxDecoration(
+                        color: Colors.white.withOpacity(0.8),
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          TextField(
+                            decoration: InputDecoration(
+                                labelText: "Email",
+                                border: UnderlineInputBorder(),
+                                focusedBorder: UnderlineInputBorder(
+                                  borderSide: BorderSide(
+                                      color: Theme.of(context).primaryColor),
+                                )),
+                          ),
+                          TextField(
+                            decoration: InputDecoration(
+                              labelText: "Mật khẩu",
+                              border: UnderlineInputBorder(),
+                              focusedBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(
+                                    color: Theme.of(context).primaryColor),
+                              ),
+                            ),
+                            obscureText: true,
+                          ),
+                          TextField(
+                            decoration: InputDecoration(
+                              labelText: "Nhập lại mật khẩu",
+                              border: UnderlineInputBorder(),
+                              focusedBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(
+                                    color: Theme.of(context).primaryColor),
+                              ),
+                            ),
+                            obscureText: true,
+                          ),
+                          const SizedBox(height: 30),
+                          ElevatedButton(
+                            onPressed: () {},
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Theme.of(context)
+                                  .colorScheme
+                                  .primary, // Màu nền
+                              foregroundColor: Theme.of(context)
+                                  .colorScheme
+                                  .onPrimary, // Màu chữ
+
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 30, vertical: 12),
+                            ),
+                            child: const Text("ĐĂNG KÝ"),
+                          ),
+                          const SizedBox(height: 40),
+                          RichText(
+                            text: TextSpan(
+                              text: "Nếu bạn đã có tài khoản hãy ",
+                              style:
+                                  TextStyle(fontSize: 14, color: Colors.black),
+                              children: const <TextSpan>[
+                                TextSpan(
+                                  text: "Đăng nhập",
+                                  style: TextStyle(
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.bold,
+                                    decoration: TextDecoration.underline,
+                                    color: Colors.black,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
