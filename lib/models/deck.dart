2ab8dc7dff9597989d613d38e5297889c1dd2a8a
DMonkey2943 <ltnduyen29042003@gmail.com>
2025-03-11 22:08:31 +0700

    update DeckModel

--- lib/models/deck.dart
+++ lib/models/deck.dart
@@ -1,18 +1,32 @@
+import 'dart:io';
+// import 'flashcard.dart';
+
 class Deck {
   final String? id;
   final String title;
   final String type;
   final String imageBg;
-  bool isFavorite;
-  final String userId;
+  final bool isFavorite;
+  // final String userId;
+  final bool isSuperUser;
+  // final List<Flashcard>? flashcards;
+  final File? imageBgFile;
+
+  // int? get flashcardCount {
+  //   return flashcards?.length;
+  // }
 
   Deck({
     this.id,
     required this.title,
-    this.type = 'yours',
-    required this.imageBg,
+    required this.type,
+    // this.type = 'yours',
+    this.imageBg = '',
     this.isFavorite = false,
-    required this.userId,
+    // required this.userId,
+    this.isSuperUser = false,
+    // this.flashcards,
+    this.imageBgFile,
   });
 
   Deck copyWith({
@@ -21,7 +35,9 @@ class Deck {
     String? type,
     String? imageBg,
     bool? isFavorite,
-    String? userId,
+    // String? userId,
+    bool? isSuperUser,
+    // List<Flashcard>? flashcards,
   }) {
     return Deck(
       id: id ?? this.id,
@@ -29,7 +45,35 @@ class Deck {
       type: type ?? this.type,
       imageBg: imageBg ?? this.imageBg,
       isFavorite: isFavorite ?? this.isFavorite,
-      userId: userId ?? this.userId,
+      // userId: userId ?? this.userId,
+      isSuperUser: isSuperUser ?? this.isSuperUser,
+      // flashcards: flashcards ?? this.flashcards,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'title': title,
+      'type': type,
+      'isFavorite': isFavorite,
+      // 'userId': userId,
+      'isSuperUser': isSuperUser,
+      // 'flashcards': flashcards.map((flashcard) => flashcard.toJson()).toList(),
+    };
+  }
+
+  factory Deck.fromJson(Map<String, dynamic> json) {
+    return Deck(
+      id: json['id'],
+      title: json['title'],
+      type: json['type'],
+      imageBg: json['imageBg'],
+      isFavorite: json['isFavorite'],
+      // userId: json['userId'],
+      isSuperUser: json['isSuperUser'],
+      // flashcards: (json['flashcards'] as List)
+      //     .map((flashcardJson) => Flashcard.fromJson(flashcardJson))
+      //     .toList(),
     );
   }
 }
